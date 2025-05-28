c1716e893e792574bcc64b9411bd00e0ff4f8aa5
Yazhini-16 <yazhmural123@gmail.com>
2025-05-29 00:16:18 +0530

    update UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,3 @@
-// src/App.js
-/*import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import Dashboard from "./pages/Dashboard";
-import CreateFlashcard from "./pages/CreateFlashcard";
-
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create" element={<CreateFlashcard />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-*/
-// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
